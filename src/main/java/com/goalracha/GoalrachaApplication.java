9ba9b05d7f0565bc3975287c80926d5553bd9cdc
opjtz <jtpark1957@gmail.com>
2024-02-28 12:07:20 +0900

    create project

---
+++ src/main/java/com/goalracha/GoalrachaApplication.java
@@ -0,0 +1,13 @@
+package com.goalracha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalrachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalrachaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goalracha/GoalrachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goalracha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalrachaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
