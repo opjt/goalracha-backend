cf3012ad25709ab26e2feca5100cf29947d23f67
opjtz <jtpark1957@gmail.com>
2024-03-13 18:28:01 +0900

    구장상세정보

--- src/main/java/com/goalracha/service/GroundServiceImpl.java
+++ src/main/java/com/goalracha/service/GroundServiceImpl.java
@@ -67,29 +67,11 @@ public class GroundServiceImpl implements GroundService {
     }
 @Override
 public Long register(GroundDTO groundDTO) {
-    Ground ground = dtoToEntity(groundDTO);
+    Ground ground = GroundDTO.dtoToEntity(groundDTO);
     Ground result = groundRepository.save(ground);
     return result.getGNo();
 }
-    private Ground dtoToEntity(GroundDTO groundDTO) {
-        Ground ground = Ground.builder().gNo(groundDTO.getGNo()).name(groundDTO.getName()).addr(groundDTO.getAddr())
-                .inAndOut(groundDTO.getInAndOut()).width(groundDTO.getWidth()).grassInfo(groundDTO.getGrassInfo())
-                .recommdMan(groundDTO.getRecommdMan()).usageTime(groundDTO.getUsageTime()).openTime(groundDTO.getOpenTime())
-                .closeTime(groundDTO.getCloseTime()).fare(groundDTO.getFare()).userGuide(groundDTO.getUserGuide())
-                .userRules(groundDTO.getUserRules()).refundRules(groundDTO.getRefundRules()).vestIsYn(groundDTO.isVestIsYn())
-                .footwearIsYn(groundDTO.isFootwearIsYn()).showerIsYn(groundDTO.isShowerIsYn()).roopIsYn(groundDTO.isRoopIsYn())
-                .airconIsYn(groundDTO.isAirconIsYn()).parkareaIsYn(groundDTO.isParkareaIsYn())
-                .build();
 
-        List<String> uploadFileNames = groundDTO.getUploadFileNames();
-        if (uploadFileNames == null) {
-            return ground;
-        }
-        uploadFileNames.stream().forEach(uploadName -> {
-            ground.registerImageName(uploadName);
-        });
-        return ground;
-    }
 
 
 
@@ -97,31 +79,11 @@ public Long register(GroundDTO groundDTO) {
     public GroundDTO get(Long gNo) {
         java.util.Optional<Ground> result = groundRepository.selectOne(gNo);
         Ground ground = result.orElseThrow();
-        GroundDTO groundDTO = entityToDTO(ground);
+        GroundDTO groundDTO = GroundDTO.entityToDTO(ground);
 
         return groundDTO;
     }
-    private GroundDTO entityToDTO(Ground ground) {
-        GroundDTO groundDTO = GroundDTO.builder().gNo(ground.getGNo()).name(ground.getName()).addr(ground.getAddr())
-                .inAndOut(ground.getInAndOut()).width(ground.getWidth()).grassInfo(ground.getGrassInfo())
-                .recommdMan(ground.getRecommdMan()).usageTime(ground.getUsageTime()).openTime(ground.getOpenTime())
-                .closeTime(ground.getCloseTime()).fare(ground.getFare()).userGuide(ground.getUserGuide())
-                .userRules(ground.getUserRules()).refundRules(ground.getRefundRules()).vestIsYn(ground.isVestIsYn())
-                .footwearIsYn(ground.isFootwearIsYn()).showerIsYn(ground.isShowerIsYn()).roopIsYn(ground.isRoopIsYn())
-                .airconIsYn(ground.isAirconIsYn()).parkareaIsYn(ground.isParkareaIsYn()).build();
-        List<GroundImage> imageList = ground.getImageList();
-        if (imageList == null || imageList.size() == 0) {
 
-            return groundDTO;
-        }
-
-        List<String> fileNameList = imageList.stream().map(productImage ->
-                productImage.getFileDirectory()).toList();
-        groundDTO.setUploadFileNames(fileNameList);
-
-        return groundDTO;
-
-    }
 
     @Override
     public void modify(GroundDTO groundDTO) {
