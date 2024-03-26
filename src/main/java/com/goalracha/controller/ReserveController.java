0eb96ff5f059e50e89ba863d3a2a0e2b685d6c32
opjt <jtpark1957@gmail.com>
2024-03-26 19:49:32 +0900

    mergefix

--- src/main/java/com/goalracha/controller/ReserveController.java
+++ src/main/java/com/goalracha/controller/ReserveController.java
@@ -10,10 +10,6 @@ import com.goalracha.repository.ReserveRepository;
 import com.goalracha.service.ReserveService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -176,7 +172,7 @@ public class ReserveController {
         return reserveService.getOwnerStatistics(uNo);
     }
 
-    @GetMapping("/v/user-reservations/{uNo}")
+    @GetMapping("/user-reservations/{uNo}")
     public ResponseEntity<PageResponseDTO<UserReserveListDTO>> getUserReservationsWithUserInfo(
             @PathVariable Long uNo, PageRequestDTO pageRequestDTO) {
         PageResponseDTO<UserReserveListDTO> responseDTO = reserveService.getUserReservationsWithUserInfo(uNo, pageRequestDTO);
